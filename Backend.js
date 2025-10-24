49b04783eb449c6b6bf84609fb0948778e7ff3c3
TafelSterkrade <tafel.sterkrade@web.de>
2025-10-24 08:31:39 +0200

    Delete Backend.js

--- Backend.js
+++
@@ -1,8 +0,0 @@
-// Backend.js (02.09.2025)
-
-//Testversion:
-//const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoGKR58bdWuOmPyKYjCJKL33g-EGz1HLoFXuBZmaQccsbo_NHjU691n0Xu_h-DiU8tGA/exec";
-
-
-//Tafelversion:
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxrlHyMIaNi1W3yQfC4Z24TTJFEZVyJG20V4fSddX0jxU5dYVb5r3J8GZh0H3LLytqB/exec";
